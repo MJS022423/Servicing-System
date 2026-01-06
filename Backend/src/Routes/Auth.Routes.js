73144aa69d536b44685910178f53d5e7ea9b7d51
乃ЕTA <TA>
2026-01-06 21:22:20 +0800

    Auth routes

--- Backend/src/Routes/Auth.Routes.js
+++ Backend/src/Routes/Auth.Routes.js
@@ -1,10 +1,10 @@
 import express from 'express';
+import Login from '../Auth/Auth.Login.js';
 import Register from './../Auth/Auth.Register.js'
 
 const Authrouter = express.Router();
 
+Authrouter.post('/Login', Login);
 Authrouter.post('/Register', Register);
-
-
 
 export default Authrouter;
