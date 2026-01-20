ff0f3ecc3143e29b2807404bffe46682794e3ad5
乃ЕTA <187012799+MJS022423@users.noreply.github.com>
2026-01-20 20:58:36 +0800

    Updated Server

--- Backend/server.js
+++ Backend/server.js
@@ -2,38 +2,32 @@ import path from 'path';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import express from 'express';
-import rateLimit from 'express-rate-limit';
 import __dirname from './src/lib/dirname.js';
 import Authrouter from './src/Routes/Auth.Routes.js'
 import AnnouncementRouter from './src/Routes/Announcement.Routes.js';
 import ReportRoutes from './src/Routes/Report.Routes.js';
-
-// Load environment variables
+import ServiceRouter from './src/Routes/ServiceRequest.Routes.js';
+import { Requestlimiter } from './src/middleware/Ratelimit.middleware.js';
 
 dotenv.config({path: path.join(__dirname,'config', '.env')});
 const app = express();
-const limiter = rateLimit({
-  windowMs: 30 * 60 * 1000,
-  limit: 100,
-  standardHeaders: true,
-  legacyHeaders: false
-});
+const PORT = process.env.Port;
 
 app.use(cors({ origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175'], credentials: true }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(express.json({ limit: '50mb' }));
-app.use(limiter);
+app.use(Requestlimiter);
 
 app.use(Authrouter);
 app.use(AnnouncementRouter);
 app.use(ReportRoutes);
+app.use(ServiceRouter);
 
 app.get('/status', (req, res) => {
   res.status(200).json({ status: 'ok', message: '[ EXPRESS SERVER IS RUNNING ]' });
   console.log('[ EXPRESS SERVER IS RUNNING ]');
 });
 
-const PORT = process.env.Port;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
